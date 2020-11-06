1be1719230050482feeb672b8e95311acb2f7831
Haydon Houghton <haydonhoughton@outlook.com>
2020-11-06 12:36:09 +0000

    Write User class from tech test PDF

---
+++ lib/user_birthday.rb
@@ -0,0 +1,18 @@
+require 'date'
+
+class User
+  attr_reader :name, :date_of_birth
+
+  def initialize(name, date_of_birth)
+    @name = name
+    @date_of_birth = date_of_birth  
+
+  end
+
+  def age
+
+  end
+
+  def next_birthday
+
+  end
